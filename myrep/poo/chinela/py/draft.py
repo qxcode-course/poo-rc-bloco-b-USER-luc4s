2f2d90dc8e00dba6f31c761e92beb491233af755
Lucas <pedrolucas994234430@gmail.com>
2025-10-07 14:57:39 +0000

    chinela

---
+++ myrep/poo/chinela/py/draft.py
@@ -0,0 +1,17 @@
+class Chinela:
+    def __init__(self, tamanho: int):
+        self.tamanho = 0
+
+    def getTamanho(self,valor: int):
+        return self.tamanho
+    
+    def setTamanho(self, valor: int):
+        if tamanho >= 20 and tamanho <= 50 and tamanho %2 == 0:
+            self.tamanho += 1
+chinela = Chinela()
+
+while True:
+    print("Insira o tamanho da sua chinela: ")
+    tamanho = int(input())
+     if tamanho >= 20 and tamanho <= 50 and tamanho %2 ==0
+     
