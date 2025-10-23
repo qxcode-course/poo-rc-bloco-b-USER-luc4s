8d8c1d26848d41deb94fc90ea71858c40dfdc161
Pedro Lucas <pedrolucas994234430@gmail.com>
2025-10-23 17:39:54 -0300

    terminando motoca

--- myrep/poo/motoca/py/draft.py
+++ myrep/poo/motoca/py/draft.py
@@ -13,4 +13,49 @@ class Pessoa:
         return f"{self._nome}:{self._idade}"
     
 class Motoca:
+    def __init__(self,potencia = 1):
+        self.potencia = potencia
+        self.time = 0
+        self.pessoa = None
+
+    def inserir(self, pessoa: Pessoa):
+        if self.pessoa is not None:
+            print("fail: busy motorcycle")
+            return False
+        self.pessoa = pessoa
+        return True
     
+    def remover(self):
+        if self.pessoa is None:
+            print("fail: empty motorcycle")
+            return None
+        pessoa = self.pessoa
+        self.pessoa = None
+        print(pessoa)
+        return pessoa
+    
+    def buyTime(self, time: int):
+        self.time += time
+
+    def drive(self, time: int):
+        if self.time <= 0:
+            print("fail: buy time first")
+            return 
+        if self.pessoa is None:
+            print("fail: empty motorcycle")
+            return
+        if self.pessoa.getIdade() > 10:
+            print("fail: too old to drive")
+            return 
+        if time > self.time:
+            print(f"fail: time finished after {self.time} minutes")
+            self.time = 0
+        else:
+            self.time -= time
+
+        def honk(self):
+            return "P" + ("e" * self.potencia) + "m"
+
+        def __str__(self):
+            pessoa_str = str(self.pessoa) if self.pessoa else "empty"
+            return f"power:{self.potencia}, time:{self.time}, person:({pessoa_str})"
